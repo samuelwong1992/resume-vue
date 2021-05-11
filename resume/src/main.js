0aee8213ce0a1d34380e7574a07c7d09ed77a08e
Samuel Wong <samuelwong1992@gmail.com>
2021-05-11 13:50:41 +1000

    Updated host name for API to new domain name

--- resume/src/main.js
+++ resume/src/main.js
@@ -7,4 +7,4 @@ import VueAxios from 'vue-axios'
 
 const app = createApp(App);
 app.use(store).use(router).use(VueAxios, axios).mount("#app");
-app.config.globalProperties.$api_hostname = "http://127.0.0.1:8001/"
+app.config.globalProperties.$api_hostname = "https://api.swongdev.com/"
